649d59f2b8233c1e83e4e03d5adc15c5c86ca53f
pp1mt <pewpew1moartime@gmail.com>
2018-04-28 21:07:04 +0800

    Patch for replayer to check for HARCODED goal. Added minimizer2

---
+++ minimizer2.py
@@ -0,0 +1,100 @@
+#!/usr/env/python
+import sys
+import subprocess
+from subprocess import PIPE
+# Custom modules
+import replayerDraft2
+import parse
+
+# Takes in action list, replays it
+# @returns: boolean result of whether goal is reached.
+def test_replay(commandList):
+    return replayerDraft2.replay_list(commandList)
+    
+
+# Take in action list from parser.
+# Partitions the commandlist into different connections
+# and replays it, checking if it reaches the goal.
+def ddmin_level1(commandList):
+    current_list_passing = test_replay(commandList)
+    if current_list_passing:
+        # this commandlist reaches the goal
+        command_len = len(commandList)
+        if command_len == 1:
+            return True
+        else:
+            granularity_n = 2
+            while command_len >= 2:
+                print command_len
+                subsets = splitTestSet(commandList, granularity_n)
+                print "[Minimizer Debug] Subsets are:"
+                for each_set in subsets:
+                    #TODO read into pcap?
+                    complement = [c for c in commandList if c not in each_set]
+                    if test_replay(complement):
+                        print "[Minimizer Debug] New goal-reaching configuration"
+                        print "New list: ", complement
+                        granularity_n = max(granularity_n-1, 2)
+                        print "[Minimizer Debug] Granularity:%d" % granularity_n
+                        complement_reaching = 0
+                        commandList = complement
+                        command_len = len(commandList)
+                        break
+                    
+                    if not complement_reaching:
+                        if granularity_n == command_len:
+                            break
+                        else:
+                            granularity_n = min(granularity_n*2,commandLen)
+                            print "[Minimizer Debug] Granularity:%d" %granularity_n
+
+                    if commandLen == 1:
+                        # Can not be divided further
+                        pass
+            print "[Minimizer Debug] Finished level 1 minimization."
+            return commandList
+    else:
+        print "[Minimizer Debug] This command list cannot reach the goal."
+    
+# Divide test case into granularity parts.
+def splitTestSet(commandList,granularity_n):
+    subsets = []
+    start = 0
+    for i in range(granularity_n):
+        subset = commandList[start:start + (len(commandList) - start) / \
+                                           (granularity_n - i)]
+        subsets.append(subset)
+        start = start + len(subset)
+    assert len(subsets) == granularity_n
+    for s in subsets:
+        assert len(s) > 0
+    return subsets
+
+def convert_pcap_to_action(pcap_file):
+    pcap_conn_dict = parse.parse_into_connections(pcap_file)
+    actions = parse.commands_from_dict_sockets(pcap_conn_dict)
+    return actions
+
+# Open pcap file
+# Write minimized result into new pcap_file
+def minimize(pcap_file):
+    action_list = convert_pcap_to_action(pcap_file)
+    # Start to minimize on the first level
+    minimized_connections = ddmin_level1(action_list)
+    # Minimize on second level
+    #TODO
+
+    print "Final configuration found. Launching capture thread to write pcap file"
+    #TODO  capture packets from another thread!
+    test_replay(minimized_connections)
+
+
+def main():
+    if len(sys.argv) != 2:
+        print ("Usage: ./minimizer2.py <pcap.file>")
+    else:
+        pcapfile = sys.argv[1]
+        minimize(pcapfile)
+    
+if __name__ == "__main__":
+    main()
--- replayerDraft2.py
+++ replayerDraft2.py
@@ -1,4 +1,4 @@
-#!/usr/env python
+#!/usr/env/python
 # Scapy
 from scapy.all import *
 # Python
@@ -9,7 +9,8 @@ import socket
 import parse
 
 RESPONSE_FROM_SERVER = parse.RESPONSE_FROM_SERVER
-
+HARDCODED_FILE_GOAL = open("server/var/www/cprogram.zip", "rb")
+HARDCODED_GOAL_CONTENTS = HARDCODED_FILE_GOAL.read()
 
 # Creates a new connection using sockets.
 # @params destination IP, Port and source port
@@ -35,42 +36,52 @@ def new_socket_connection(dip,dp,sp):
 # @returns the reply that the socket received
 def wait_receive(sock):
     print ("[DEBUG] SERVER RESPONSE WAIT")
-    reply = sock.recv(4096)
+    reply = sock.recv(8192)
+    time.sleep(1)
     print ("[DEBUG] RESPONSE RECEIVED")
-    print reply
-    return reply
+    is_goal= False
+    if HARDCODED_GOAL_CONTENTS in reply:
+        is_goal = True
+        print "[DEBUG] GOAL IS REACHED"
+        print is_goal
+    print reply 
+    return is_goal
 
-# Replays a PCAP file [BUGGED, DO NOT USE]
+# Replays a PCAP file 
 # Translates it into commands in the form of Actions Lists and
 # creates sockets, wait for replies and sends payloads.
 def replay_PCAP(pcaps):
     conn_dict = parse.parse_into_connections(pcaps)
     traffic_list = parse.commands_from_dict_sockets(conn_dict)
-    
-    packetlist = []
+    replay_list(traffic_list)
+
+# Replays an action list of commands
+# Similarly, creates sockets, wait for replies and sends payloads.
+def replay_list(traffic_list):
+    is_reaching_goal = False
+
     for traffic in traffic_list:
-        time.sleep(1)
-        Dest = traffic[0].split(",")
-        SPort = traffic[0].split(",")[2]
+        #time.sleep(1)
+        if not is_reaching_goal:
+            Dest = traffic[0].split(",")
+            SPort = traffic[0].split(",")[2]
         
-        DIP = Dest[0].split(":")[0]
-        DPort = Dest[0].split(":")[1]
+            DIP = Dest[0].split(":")[0]
+            DPort = Dest[0].split(":")[1]
         
-        #sniff_filters = "((ip src host %s and dst host %s) and (src port %s or dst port %s)) and not ip multicast" % (DIP, DIP, SPort, DPort)
-        #conn_sniff = sniff(filter=sniff_filters, count=10)
-        conn = new_socket_connection(DIP, DPort, SPort)
-        for item in traffic[1:]:
-            if item == RESPONSE_FROM_SERVER:
-                # todo: settle extra ack from handshake
-                wait_receive(conn)
-            else:
-                print ("[DEBUG] SENDING PAYLOAD" + item)
-                try:
-                    conn.send(item)
-                    print ("[DEBUG] Payload sent!")
-                except Exception as e:
-                    print e
-                    
+            conn = new_socket_connection(DIP, DPort, SPort)
+            for item in traffic[1:]:
+               if item == RESPONSE_FROM_SERVER:
+                    # todo: settle extra ack from handshake
+                    is_reaching_goal = wait_receive(conn)
+               else:
+                    print ("[DEBUG] SENDING PAYLOAD" + item)
+                    try:
+                        conn.send(item)
+                        print ("[DEBUG] Payload sent!")
+                    except Exception as e:
+                        print e
+    return is_reaching_goal
 
 def main():
     if len(sys.argv) != 2:
